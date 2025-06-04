07ff38e25c97a05e6654e1779461b97fc4a420ae
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-06-04 14:15:32 +0530

    random contact handling updated as per indian phone numbers

--- src/utils/randomContact.ts
+++ src/utils/randomContact.ts
@@ -1,8 +1,12 @@
 export function generateRandomPhoneNumber(): string {
+    const startingDigits = ['9', '8'];
     const digits = '0123456789';
-    let number = '';
-    for (let i = 0; i < 10; i++) {
+
+    let number = startingDigits[Math.floor(Math.random() * startingDigits.length)];
+
+    for (let i = 0; i < 9; i++) {
         number += digits.charAt(Math.floor(Math.random() * digits.length));
     }
+
     return number;
 }
